f662c796e2d720b2f79a111a4ef112984deef9d2
xxyzz <gitpull@protonmail.com>
2021-01-15 22:43:31 +0800

    week1 problem 7

---
+++ week1/problem7.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+'''
+Suppose you are told that the one time pad encryption of the message
+"attack at dawn" is 09e1c5f70a65ac519458e7e53f36
+
+(the plaintext letters are encoded as 8-bit ASCII and the given ciphertext
+ is written in hex). What would be the one time pad encryption of the
+ message "attack at dusk" under the same OTP key?
+'''
+
+
+def str_to_int(s):
+    return int("".join([hex(ord(c))[2:] for c in s]), base=16)
+
+
+key = str_to_int("attack at dawn") ^ 0x09e1c5f70a65ac519458e7e53f36
+
+print(hex(str_to_int("attack at dusk") ^ key))
